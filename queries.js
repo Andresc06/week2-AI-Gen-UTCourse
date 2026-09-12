f392a7a0f1a096937c00bf86c0798f8b37ef36fa
Andresc06 <andrescontrerasalvarez066@gmail.com>
2026-09-12 12:01:12 -0500

    feat: Implement queries from 5 to 8

--- queries.js
+++ queries.js
@@ -28,12 +28,63 @@ async function query4() {
     console.log(threads);
 }
 
+async function query5() {
+    const author = await User.findOne({"name": "Ethan"})
+    const subrredit =  await Subreddit.findOne({"name": "devops"})
+
+    const thread = await Thread.create({
+      title: "Docker or kurbenetes",
+      content: "Which one is better?",
+      author: author._id,
+      subreddit: subrredit._id,
+      createdAt: new Date()
+    })
+    console.log(thread)
+}
+
+async function query6() {
+    const thread = await Thread.findOne({ title: "Docker or kurbenetes" });
+    thread.title = "Facebook or Snapchat";
+    await thread.save();
+    console.log(thread);
+}
+
+async function query7() {
+    const subreddits = await Subreddit.find({});
+    for(const sub of subreddits) {
+      const deletedThreads = await Thread.deleteMany({
+        subreddit: sub._id
+      })
+      console.log(deletedThreads.deletedCount);
+    }
+    
+    await Subreddit.deleteMany({});
+    console.log('deleted all subreddits and their associated threads');
+}
+
+// Find the author ID and thread count for the user who posted the most threads
+async function query8() {
+    const threads = await Thread.aggregate([
+        { $group: { _id: "$author", threadCount: { $sum: 1 } } },
+        { $sort: { threadCount: -1 } },
+        { $limit: 1 }
+    ]);
+    
+    const topAuthorId = threads[0]._id;
+    const topThreadCount = threads[0].threadCount;
+    console.log(`Author ID: ${topAuthorId}, Thread Count: ${topThreadCount}`);
+}
+
 async function runQueries() {
     // Uncomment the query you want to run
     // await query1();
     // await query2();
     // await query3();
-    await query4();
+    // await query4();
+    // await query5();
+    // await query6();
+    // await query7();
+    await query8();
 }
 
 async function main() {
